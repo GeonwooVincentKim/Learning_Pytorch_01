fdae073cd4f1aed9e293fa66e996554ad0a15815
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-08 12:27:01 +0900

    Calculating Differential Variable

---
+++ ANN_Autograd/Autograd.py
@@ -0,0 +1,15 @@
+import torch
+# Define Scalar Tensor named "w"
+# Meaning of requires_grad is Calculating Differential Variable.
+w = torch.tensor(1.0, requires_grad=True)
+a = w * 3
+
+# More Complicated Calculate.
+l = a**2
+
+# By calling the backward() Function on L,
+# w.grad returned the formula to which "w" belongs
+# to the derivative of "w", 18.
+l.backward()
+print("A Value in which 'l' "
+      "is Differential by 'w' is ({})".format(w.grad))
