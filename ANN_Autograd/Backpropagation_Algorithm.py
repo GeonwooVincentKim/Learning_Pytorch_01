0b6814d062254bd57f4f215d9ea4f66dea03edda
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-17 21:32:54 +0900

    Added What does Cluster which is using for BackPropagation_Algorithm standing for

--- ANN_Autograd/Backpropagation_Algorithm.py
+++ ANN_Autograd/Backpropagation_Algorithm.py
@@ -3,3 +3,26 @@
 import torch
 import numpy
 from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plot
+import torch.nn.functional as F
+
+
+# Number of Dimension
+n_dim = 2
+
+# Now you make Clusters to find out where cluster location
+# which is included data .
+# Make Training Value which is defined as X and Y.
+# make_blobs() is a function which is imported from Scikit-Learn Library.
+x_train, y_train = make_blobs(
+    n_samples=80, n_features=n_dim,
+    centers=[[1, 1], [-1, -1], [1, -1], [-1, 1]],
+    shuffle=True, cluster_std=0.3
+)
+
+# Make Testing Value which is defined as X and Y.
+x_test, y_test = make_blobs(
+    n_samples=20, n_features=n_dim,
+    centers=[[1, 1], [-1, -1], [1, -1], [-1, 1]],
+    shuffle=True, cluster_std=0.3
+)
