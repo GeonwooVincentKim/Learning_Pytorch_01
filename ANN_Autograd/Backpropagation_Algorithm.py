9ed5b0020859af50ea7abc05edda4aefec315e10
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-18 12:52:48 +0900

    Added some function related to Backpropagation Algorithm

--- ANN_Autograd/Backpropagation_Algorithm.py
+++ ANN_Autograd/Backpropagation_Algorithm.py
@@ -3,9 +3,8 @@
 import torch
 import numpy
 from sklearn.datasets import make_blobs
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import torch.nn.functional as F
-
 
 # Number of Dimension
 n_dim = 2
@@ -26,3 +25,38 @@ x_test, y_test = make_blobs(
     centers=[[1, 1], [-1, -1], [1, -1], [-1, 1]],
     shuffle=True, cluster_std=0.3
 )
+
+
+# Implement label_map() function to change all of the Data
+# which have No.0 or No.1 as a Label to No.0 Label.
+def label_map(y_, from_, to_):
+    y = numpy.copy(y_)
+    for f in from_:
+        y[y_ == f] = to_
+    return y
+
+
+y_train = label_map(y_train, [0, 1], 0)
+y_train = label_map(y_train, [2, 3], 1)
+y_test = label_map(y_test, [0, 1], 0)
+y_test = label_map(y_test, [2, 3], 1)
+
+
+# Visualization Data which is Labeled well or not
+# by using Matplotlib Library.
+def vis_data(x, y=None, c='r'):
+    if y is None:
+        y = [None] * len(x)
+    for x_, y_ in zip(x, y):
+        # If Label Number is 0, it shows '.',
+        # else Label Number is 1, it shows '+'.
+        if y_ is None:
+            plt.plot(x_[0], x_[1], '*', markerfacecolor='none',
+                     markeredgecolor=c)
+        else:
+            plt.plot(x_[0], x_[1], c+'o' if y_ == 0 else c+'+')
+
+
+plt.figure()
+vis_data(x_train, y_train, c='r')
+plt.show()
