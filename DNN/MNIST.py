7be3317878528c96a0940bbf1149f983deb1013c
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-22 20:59:10 +0900

    DNN Project for MNIST Project

---
+++ DNN/MNIST.py
@@ -0,0 +1,23 @@
+from torchvision import datasets, transforms, utils
+from torch.utils import data
+"""
+  1. torch.utils.data
+  --> It's a module that have tools which is defines
+  Dataset standard and get them by cutting and mixing together.
+  
+  2. torchvision.datasets
+  --> Collection of Image Data Set that inherit 
+  'torch.utils.data.Dataset'.
+  
+  3. torchvision.transform
+  --> This module contains various conversion filters 
+  that can be used for Image Dataset.
+  
+  4. torchvision.utils
+  --> It's a module that have tools for save and
+  visualization Image Data.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
