7a572d1bfd786868ec941155aea939f2b06cae11
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-23 21:46:04 +0900

    Starting to code MNIST Project

--- DNN/MNIST.py
+++ DNN/MNIST.py
@@ -1,5 +1,6 @@
 from torchvision import datasets, transforms, utils
 from torch.utils import data
+
 """
   1. torch.utils.data
   --> It's a module that have tools which is defines
@@ -21,3 +22,34 @@ from torch.utils import data
 import matplotlib.pyplot as plt
 import numpy as np
 
+"""
+  1. Convert Image to Tensor.
+  2. The version of transform is a tool that 
+  convert inputs.
+"""
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+"""
+  1. The Fashion MNIST Dataset is divided 
+  into a training set for training and 
+  a test set for performance evaluation.
+  
+  2. Check Data-sets which you define folder location
+  as 'root', if Data-sets not existing, 
+  it will save New Data-sets file automatically.
+"""
+trainset = datasets.FashionMNIST(
+    root='./.data/',
+    train=True,
+    download=True,
+    transform=transform
+)
+
+testset = datasets.FashionMNIST(
+    root='./.data/',
+    train=False,
+    download=True,
+    transform=transform
+)
