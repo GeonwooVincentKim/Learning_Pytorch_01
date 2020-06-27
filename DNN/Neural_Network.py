509b35f5352401348866f738cf4f7cd0ad9debde
GeonwooVincentKim <kdsnop@gmail.com>
2020-06-27 20:03:01 +0900

    Added Training Process by using EPOCHS

--- DNN/Neural_Network.py
+++ DNN/Neural_Network.py
@@ -14,6 +14,48 @@ EPOCHS = 30
 BATCH_SIZE = 64
 
 """
+  1. Convert Image to Tensor.
+  2. The version of transform is a tool that 
+  convert inputs.
+"""
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+"""
+  1. The Fashion MNIST Dataset is divided 
+  into a training set for training and 
+  a test set for performance evaluation.
+
+  2. Check Data-sets which you define folder location
+  as 'root', if Data-sets not existing, 
+  it will save New Data-sets file automatically.
+"""
+trainset = datasets.FashionMNIST(
+    root='./.data/',
+    train=True,
+    download=True,
+    transform=transform
+)
+
+testset = datasets.FashionMNIST(
+    root='./.data/',
+    train=False,
+    download=True,
+    transform=transform
+)
+
+train_loader = torch.utils.data.DataLoader(
+    dataset=trainset,
+    batch_size=BATCH_SIZE
+)
+
+test_loader = torch.utils.data.DataLoader(
+    dataset=testset,
+    batch_size=BATCH_SIZE
+)
+
+"""
   1. Starting for Image classification.
   2. Implement ANN (Artificial Neural Network) for
      Image Classification.
@@ -54,7 +96,7 @@ model = Net().to(DEVICE)
 
 # Use optim.SGD algorithm which is the Optimization Algorithm
 # in Pytorch embedded modules.
-optimizer = optim.SGD(model.paraemeters(), lr=0)
+optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 
 # Training occurs by repeating the tasks of viewing data
@@ -89,7 +131,38 @@ def evaluate(model, test_loader):
     test_loss = 0
     correct = 0
 
+    # Specify torch.no_grad as like this syntax.
+    # Make sure you have to send as DEVICE, and then
+    # Get output model predictive value.
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(DEVICE), target.to(DEVICE)
             output = model(data)
+
+            # Add all of Error.
+            # set 'reduction='sum'' through cross entropy for evaluate,
+            # and get Mini_Batch's 'sum' instead of Mini_Batch's 'average'.
+            test_loss += F.cross_entropy(output, target,
+                                         reduction='sum').item()
+
+            # The Class which have most big value is prediction of Model.
+            # Compare prediction and Answer and then add 1 when both of them are match.cc
+            # 'eq()' function outputs 1 when the Model Prediction Fashion Item and Label are match,
+            # otherwise, it outputs 0 when it is not Match.
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    # Now we got Error of Entire DataSet and Sum of all values of Numbers.
+    # Now we are going to get accuracy by multiply 100 to entire Answer Average.
+    # Average = Values which are contains Entire of Model DataSet and All values of Numbers.
+    test_loss /= len(test_loader.dataset)
+    test_accuracy = 100. * correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+
+# Repeat the verification using Epoch, learning, and DataSet and print the results.
+for epoch in range(1, EPOCHS + 1):
+    train(model, train_loader, optimizer)
+    test_loss, test_accuracy = evaluate(model, test_loader)
+
+    print('[{}] Test Loss : {:.4f}, Accuracy: {:.2f}%'.format(epoch, test_loss, test_accuracy))
