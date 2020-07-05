0025706e90675f1793eea4e8b8850f9b638278d0
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-05 17:35:21 +0900

    Added Torch Modules and show directory location by saving files and to test files.

--- CNN/Basic_CNN.py
+++ CNN/Basic_CNN.py
@@ -1,6 +1,37 @@
 import torch
-import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.utils.data as torch_dt
+from torchvision import transforms, datasets
 
+USE_CUDA = torch.cuda.is_available()
+DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
+
+EPOCHS = 40
+BATCH_SIZE = 64
+
+train_loader = torch_dt.DataLoader(
+    datasets.FashionMNIST(
+        './.data',
+        train=True,
+        download=True,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+    batch_size=BATCH_SIZE, shuffle=True),
+test_loader = torch_dt.DataLoader(
+    datasets.FashionMNIST(
+        './.data',
+        train=False,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307, ), (0.3801,))
+        ])
+    ),
+    batch_size=BATCH_SIZE, shuffle=True
+)
 """
   CNN Basic
 """
