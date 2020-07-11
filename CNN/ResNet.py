1655b64223ef9f7a153fc0f7606b119a27a36a1a
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-11 22:20:27 +0900

    Added ResNet train_loader and test_loader variable funciton which includes DataLoader Attribute from torch.

--- CNN/ResNet.py
+++ CNN/ResNet.py
@@ -17,3 +17,28 @@ from torchvision import transforms, datasets
 
 EPOCHS = 300
 BATCH_SIZE = 128
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10('./.DATA',
+                     train=True,
+                     download=True,
+                     transform=transforms.Compose([
+                         transforms.RandomCrop(32, padding=4),
+                         transforms.RandomHorizontalFlip(),
+                         transforms.ToTensor(),
+                         transforms.Normalize((0.5, 0.5, 0.5),
+                                              (0.5, 0.5, 0.5))
+                     ])),
+    batch_size=BATCH_SIZE, shuffle=True
+)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10('./.DATA',
+                     train=False,
+                     transform=transforms.Compose([
+                         transforms.ToTensor(),
+                         transforms.Normalize((0.5, 0.5, 0.5),
+                                              (0.5, 0.5, 0.5))
+                     ])),
+    batch_size=BATCH_SIZE, shuffle=True
+)
