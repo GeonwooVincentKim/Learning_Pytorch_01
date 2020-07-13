20c354b9efc94c9b80cf6a2ff13012159bc651ea
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-13 18:43:36 +0900

    Added ResNet Classes

--- CNN/ResNet.py
+++ CNN/ResNet.py
@@ -94,3 +94,22 @@ class BasicBlock(nn.Module):
         out += self.shortcut(x)
         out = F.relu(out)
         return out
+
+
+"""
+    Defined ResNet.
+"""
+
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.layer1 = self._make_layer(16, 2, stride=1)
+        self.layer2 = self._make_layer(32, 2, stride=2)
+        self.layer3 = self._make_layer(64, 2, stride=2)
+        self.linear = nn.Linear(64, num_classes)
