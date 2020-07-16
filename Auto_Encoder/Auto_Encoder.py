a624c526c9fd1c2f1449ce2344ad985cd76995c3
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-16 22:06:58 +0900

    Added './.data/' File Directory by added some Pytorch code.

--- Auto_Encoder/Auto_Encoder.py
+++ Auto_Encoder/Auto_Encoder.py
@@ -68,7 +68,7 @@ class Autoencoder(nn.Module):
             nn.ReLU(),
             nn.Linear(64, 128),
             nn.ReLU(),
-            nn.Linear(128, 28*28),
+            nn.Linear(128, 28 * 28),
             nn.Sigmoid(),
         )
 
@@ -89,3 +89,49 @@ class Autoencoder(nn.Module):
 autoencoder = Autoencoder().to(DEVICE)
 optimizer = torch.optim.Adam(autoencoder.parameters(), lr=0.005)
 criterion = nn.MSELoss()
+
+# Visualization an Original Image (First Column)
+view_data = trainset.data[:5].view(-1, 28 * 28)
+view_data = view_data.type(torch.FloatTensor) / 255.
+
+
+def train(autoencoder, train_loader):
+    autoencoder.train()
+    for step, (x, label) in enumerate(train_loader):
+        x = x.view(-1, 28 * 28).to(DEVICE)
+        y = x.view(-1, 28 * 28).to(DEVICE)
+        label = label.to(DEVICE)
+
+        encoded, decoded = autoencoder(x)
+
+        loss = criterion(decoded, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+for epoch in range(1, EPOCH + 1):
+    train(autoencoder, train_loader)
+
+    # Visualization a Image that comes from Decoder.
+    test_x = view_data.to(DEVICE)
+    _, decoded_data = autoencoder(test_x)
+
+    # Compare between Original Image and Result of Decoded File.
+    f, a = plt.subplots(2, 5, figsize=(5, 2))
+    print("[Epoch {}]".format(epoch))
+
+    # Set CMap Color as gray.
+    for i in range(5):
+        img = np.reshape(view_data.data.numpy()[i], (28, 28))
+        a[0][i].imshow(img, cmap='gray')
+        a[0][i].set_xticks(())
+        a[0][i].set_yticks(())
+
+    for i in range(5):
+        img = np.reshape(decoded_data.to("cpu").data.numpy()[i], (28, 28))
+        a[1][i].imshow(img, cmap='gray')
+        a[1][i].set_xticks(())
+        a[1][i].set_yticks(())
+
+    plt.show()
