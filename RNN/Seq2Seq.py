b4c86a04e3537081fa1ff881c04dbb38e72a0ff5
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-24 21:36:50 +0900

    Now we can predict Next-Syllable(Next-Letter) by using 'Teacher-Forcing' and 'Output of Decoder'.

--- RNN/Seq2Seq.py
+++ RNN/Seq2Seq.py
@@ -32,6 +32,23 @@ class Seq2Seq(nn.Module):
         decoder_state = encoder_state
         decoder_input = torch.LongTensor([0])
 
+        # Predict 'o' Token by using 'h' Token in next Iterators
+        # when you predicted "h" token from "hola" translated sentences
+        # by using ASCII Number '0', the Initial placed of Sentences Token.
+        outputs = []
+        for i in range(targets.size()[0]):
+            decoder_input = self.embedding(decoder_input).unsqueeze(1)
+            decoder_output, decoder_state = self.decoder(decoder_input, decoder_state)
+
+            # Predict Next syllable by Output of Decoder.
+            projection = self.project(decoder_output)
+            outputs.append(projection)
+
+            # Update Decoder Input by using 'Teacher-Forcing'.
+            decoder_input = torch.LongTensor([targets[i]])
+        outputs = torch.stack(outputs).squeeze()
+        return outputs
+
     def _init_state(self, batch_size=1):
         weight = next(self.parameters()).data
         return weight.new(self.n_layers, batch_size, self.hidden_dim).zero_()
