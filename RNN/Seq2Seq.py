5070052c4846d38c216a21880cdd004cf38beeb3
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-24 17:39:01 +0900

    Created Seq2seq.py file.

---
+++ RNN/Seq2Seq.py
@@ -0,0 +1,8 @@
+"""
+    Seq2Seq is the model which connected two RNN
+    models each playing different roles.
+"""
+import torch
+import torch.nn as nn
+import random
+import matplotlib.pyplot as plt
