45e5c7a9a01013d508db78e69b7c904d4abd5bf5
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-25 21:48:41 +0900

    Add comments about what does 'BICUBIC' stands for and how to apply to ML(Machine Learning) and DL(Deep Learning) Model.

--- Adversarial_Example/FSGM.py
+++ Adversarial_Example/FSGM.py
@@ -18,4 +18,27 @@ model.eval()
 CLASSES = json.load(open('./imagenet_samples/imagenet_classes.json'))
 idx2class = [CLASSES[str(i)] for i in range(1000)]
 
+# Import Image
 img = Image.open("imagenet_samples/corgie.jpg")
+
+# Convert Image as Tensor.
+"""
+    BICUBIC
+    -1. BICUBIC interpolation is an extension of cubic
+    interpolation for interpolating data points on a 
+    two-dimensional regular grid.
+    
+    -2. BICUBIC is also well used while Developer or User 
+    proceed Image Scaling.
+    
+    -3. Meaning of Image Scaling are refers to the resizing
+    of a digital image.
+    
+    -4. Whatever, BICUBIC Interpolation is a 2D system of using
+    cubic splines or other polynomial technique and enlarging
+    digital Images.
+"""
+img_transforms = transforms.Compose([
+    transforms.Resize((224, 224), Image.BICUBIC),
+    transforms.ToTensor(),
+])
