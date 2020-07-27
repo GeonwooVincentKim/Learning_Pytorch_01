ff90e88b9b78e9e89ca1726c1ea63a6217b11d4a
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-27 18:16:54 +0900

    Update 'Discriminator Class'.

--- GAN/cGAN.py
+++ GAN/cGAN.py
@@ -53,3 +53,28 @@ class Generator(nn.Module):
         c = self.embed(labels)
         x = torch.cat([z, c], 1)
         return self.model(x)
+
+
+# Discriminator
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embed = nn.Embedding(10, 10)
+        self.model = nn.Sequential(
+            nn.Linear(794, 1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x, labels):
+        c = self.embed(labels)
+        x = torch.cat([x, c], 1)
+        return self.model(x)
