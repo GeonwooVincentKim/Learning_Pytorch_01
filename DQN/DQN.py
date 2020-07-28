68e9fec7af4524ba5096fcce47192c9df5e06a0a
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-28 15:49:36 +0900

    Added 'DQN.py' and explained about what does DQN stands for and how it works.

---
+++ DQN/DQN.py
@@ -0,0 +1,13 @@
+"""
+    1. What does 'reinforcement learning' stands for.
+    - The field of machine learning which grows in the direction
+    of getting good grades by interacting with a given environment
+    is named as 'Reinforcement-Learning.'
+
+    2. Reinforcement-Learning could be divide as
+    - 1. State
+    - 2. Agent
+    - 3. Action
+    - 4. Reward
+    as 4 elements.
+"""
