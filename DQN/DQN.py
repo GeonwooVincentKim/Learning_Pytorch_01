95f0d7618db773eaa17c155d201f9fef0ea22e29
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-28 16:08:44 +0900

    Add comments that what does DQN do and introduce each part of DQN element into this project.

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -3,11 +3,13 @@
     - The field of machine learning which grows in the direction
     of getting good grades by interacting with a given environment
     is named as 'Reinforcement-Learning.'
+    as 4 elements.
 
     2. Reinforcement-Learning could be divide as
-    - 1. State
-    - 2. Agent
-    - 3. Action
-    - 4. Reward
-    as 4 elements.
+    - 1. State - AI Player.
+    - 2. Agent - It is a stage that Agent trying to find Solution.
+    - 3. Action - It is interacting with a given environment
+    (or normal Environment not a given environment) that Agent
+    enforce in the Environment.
+    - 4. Reward - It is a result or score by Agent's action.
 """
